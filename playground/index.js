15f0d80fc027b1ac48b2222070aab7a90bece43e
Piero Cascio <piero.cascio@gmail.com>
2020-10-04 10:11:38 +0200

    Example n. 2. Final implementation.

--- playground/index.js
+++ playground/index.js
@@ -1,16 +1,27 @@
 console.log(`Before`)
 
-getUser(1, (user) => {
-	console.log('User', user);
-	getRepositories(user.bitBucketUsername, (repos) => {
-		console.log(`Repos`, repos )
-		getCommits(repos, (commits) => {
-			console.log('Commits:' ,commits);
-		});
-	})
-});
+getUser(1,
+	(user) => {
+		getRepositories(user.bitBucketUsername,
+			(repos) => {
+				getCommits(repos[0],
+					(commits) => {
+						console.log('Commits:', commits);
+					});
+			})
+	});
 
 console.log(`After`)
+
+//<editor-fold desc="Synchronous implementation">
+// const user = getUser(1)
+// const repos = getRepositories(user.bitBucketUsername)
+// const commits = getCommits(repos[0]);
+// console.log('User', user);
+// console.log('Repos', repos);
+// console.log('Commits', commits);
+//</editor-fold>
+
 function getUser(id, callback) {
 	setTimeout(() => {
 		console.log(`Reading a user from a database...`)
