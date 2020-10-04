6b255ddf20f59c412d299a15f17eede4dd0612c2
Piero Cascio <piero.cascio@gmail.com>
2020-10-04 09:46:46 +0200

    Example n. 2. Using a callback.

--- playground/index.js
+++ playground/index.js
@@ -1,14 +1,14 @@
 console.log(`Before`)
-const user = getUser(1);
+getUser(1, (user => {
+	console.log('User', user);
+}));
 console.log(user);
 console.log(`After`)
 
-function getUser(id) {
-//<editor-fold desc="Asynchronous call">
+function getUser(id, callback) {
 	setTimeout(() => {
 		console.log(`Reading a user from a database...`)
-		return {id: id, bitBucketUsername: 'piero.cascio'}
+		callback({id: id, bitBucketUsername: 'piero.cascio'})
 	}, 2000)
-//</editor-fold>
-// 		return {id: id, bitBucketUsername: 'piero.cascio'}		// Add this later to
 }
+
