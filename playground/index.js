7df83ecb58ac231a83873ea668d1f82510b61699
Piero Cascio <piero.cascio@gmail.com>
2020-10-04 09:36:09 +0200

    Example n. 2.

--- playground/index.js
+++ playground/index.js
@@ -1,7 +1,14 @@
 console.log(`Before`)
-//<editor-fold desc="Asynchronous call">
-setTimeout(() => {
-	console.log(`Make a remote call...`)
-}, 2000)
-//</editor-fold>
+const user = getUser(1);
+console.log(user);
 console.log(`After`)
+
+function getUser(id) {
+//<editor-fold desc="Asynchronous call">
+	setTimeout(() => {
+		console.log(`Reading a user from a database...`)
+		return {id: id, bitBucketUsername: 'piero.cascio'}
+	}, 2000)
+//</editor-fold>
+// 		return {id: id, bitBucketUsername: 'piero.cascio'}		// Add this later to
+}
