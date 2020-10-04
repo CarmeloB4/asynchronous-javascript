c6e290ce12a675d90eb458df910b3c7fa7742eb5
Piero Cascio <piero.cascio@gmail.com>
2020-10-04 11:16:27 +0200

    Solution using Promises.

--- playground/index.js
+++ playground/index.js
@@ -1,34 +1,49 @@
 console.log(`Before`)
 
-getUser(1,
-	(user) => {
-		getRepositories(user.bitBucketUsername,
-			(repos) => {
-				getCommits(repos[0],
-					(commits) => {
-						console.log('Commits:', commits);
-					});
-			})
-	});
+// PROMISES implementation
+getUser(1)
+	.then((user) => getRepositories(user.bitBucketUsername))
+	.then((repos) => getCommits(repos))
+	.then((commits) => console.log('Commits: ', commits))
+	.catch((error) => console.error('Error: ', error.message))
+
+// CALLBACK HELL implementation
+// getUser(1,
+// 	(user) => {
+// 		getRepositories(user.bitBucketUsername,
+// 			(repos) => {
+// 				getCommits(repos[0],
+// 					(commits) => {
+// 						console.log('Commits:', commits);
+// 					});
+// 			})
+// 	});
 
 console.log(`After`)
 
-function getUser(id, callback) {
-	setTimeout(() => {
-		console.log(`Reading a user from a database...`)
-		callback({id: id, bitBucketUsername: 'piero.cascio'})
-	}, 2000)
+function getUser(id) {
+	return new Promise(((resolve, reject) => {
+		setTimeout(() => {
+			console.log(`Reading a user from a database...`)
+			resolve({id: id, bitBucketUsername: 'piero.cascio'})
+		}, 2000)
+
+	}))
 }
 
-function getRepositories(username, callback) {
-	setTimeout(() => {
-		console.log(`Calling the BitBucket API....`)
-		callback(['repo1', 'repo2', 'repo3'])
-	})
+function getRepositories(username) {
+	return new Promise(((resolve, reject) => {
+		setTimeout(() => {
+			console.log(`Calling the BitBucket API for user: ${username}....`)
+			resolve(['repo1', 'repo2', 'repo3'])
+		})
+	}))
 }
 
-function getCommits(repos, callback) {
-	setTimeout(() => {
-		callback(['commit1'])
-	}, 200)
+function getCommits(repos) {
+	return new Promise(((resolve, reject) => {
+		setTimeout(() => {
+			resolve(['commit1'])
+		}, 200)
+	}))
 }
